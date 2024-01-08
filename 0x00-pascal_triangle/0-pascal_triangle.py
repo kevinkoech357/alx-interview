39c1da59b4becbace0d2793b943dd1336109d412
Kevin_Koech <kevinkoech@proton.me>
2024-01-08 21:36:58 +0300

    returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This file contains a function that creates
+a pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Creates pascals triangle.
+    """
+    results = []
+
+    if n <= 0:
+        return {}
+
+    for _ in range(n):
+        row = [1]
+        if results:
+            last_row = results[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        results.append(row)
+    return results
