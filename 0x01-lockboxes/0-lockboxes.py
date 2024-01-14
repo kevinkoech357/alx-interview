bd6ac95e45da0a90d904eb285b998c206d2f72cc
Kevin Koech <kevinkoech@proton.me>
2024-01-14 22:49:47 +0300

    fix err

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -19,12 +19,17 @@ def canUnlockAll(boxes):
     # List to keep track of keys found during the process
     keys_found = [0]
 
+    # Set to keep track of visited boxes
+    visited = set()
+    visited.add(0)
+
     while keys_found:
         current_key = keys_found.pop()
 
         # Check if the current key opens any new boxes
         for key in boxes[current_key]:
-            if not unlocked[key]:
+            if key not in visited:
+                visited.add(key)
                 unlocked[key] = True
                 keys_found.append(key)
 
