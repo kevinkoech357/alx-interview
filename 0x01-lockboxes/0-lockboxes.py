9f733aaed6c325fc0f7ccc8a26f33eb957a5f070
Kevin Koech <kevinkoech@proton.me>
2024-01-14 22:45:17 +0300

    check if all boxes can be unlocked by a key set in another box

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Define a function that determines if all the
+boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determine if all locked boxes can be opened.
+    """
+    n = len(boxes)
+
+    # List to keep track of the unlocked status of each box
+    unlocked = [False] * n
+    unlocked[0] = True  # The first box is initially unlocked
+
+    # List to keep track of keys found during the process
+    keys_found = [0]
+
+    while keys_found:
+        current_key = keys_found.pop()
+
+        # Check if the current key opens any new boxes
+        for key in boxes[current_key]:
+            if not unlocked[key]:
+                unlocked[key] = True
+                keys_found.append(key)
+
+    # Check if all boxes are unlocked
+    return all(unlocked)
