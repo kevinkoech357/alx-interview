a4f49ca0c3f2a8ab3276d1c414271505fcc09fc5
Kevin Koech <kevinkoech@proton.me>
2024-01-17 18:36:49 +0300

    find minimum no of operations required to complete a task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+This file contains a function that attempts to
+find the minimum operations needed to perform a specified task.
+"""
+
+
+def minOperations(n):
+    """
+    Use the concept of prime factorization to determine the minimum
+    number of operations needed to achieve exactly n H characters.
+    """
+    if n <= 1:
+        return 0
+
+    def prime_factors(n):
+        i = 2
+        factors = []
+        while i * i <= n:
+            if n % i:
+                i += 1
+            else:
+                n //= i
+                factors.append(i)
+        if n > 1:
+            factors.append(n)
+        return factors
+
+    factors = prime_factors(n)
+
+    if not factors:
+        return 0
+
+    operations = 0
+    for factor in factors:
+        operations += factor
+
+    return operations
