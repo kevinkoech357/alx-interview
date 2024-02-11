293c05537cc9f1e450dbea1d954e024f8e50a430
Kevin Koech <kevinkoech@proton.me>
2024-02-11 22:37:30 +0300

    revert to prev

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -39,18 +39,18 @@ def main():
     Check if cli arg passes set requirements.
     """
     if len(sys.argv) != 2:
-        print("Usage: nqueens N", file=sys.stderr)
-        exit(1)
+        print("Usage: nqueens N")
+        sys.exit(1)
 
     try:
         n = int(sys.argv[1])
     except ValueError:
-        print("N must be a number", file=sys.stderr)
-        exit(1)
+        print("N must be a number")
+        sys.exit(1)
 
     if n < 4:
-        print("N must be at least 4", file=sys.stderr)
-        exit(1)
+        print("N must be at least 4")
+        sys.exit(1)
 
     solutions = solve_n_queens(n, 0)
     if not solutions:
